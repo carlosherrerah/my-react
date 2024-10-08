ff32916e9620d0d90d8470d46c99f3802bde0197
carlosherrerah <carlos.herrera@upa.edu.mx>
2024-10-08 13:57:55 -0600

    titulo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload mi amigo.
+          Edit <code>src/App.js</code> and save to reload mi friend.
         </p>
         <a
           className="App-link"
