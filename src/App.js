dd939da785121b706a8e8e38952385c1c4f16666
carlosherrerah <carlos.herrera@upa.edu.mx>
2024-10-08 15:02:13 -0600

    Modificar texto

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload mi friend.
+          Edit <code>src/App.js</code> and save to reload El codigo UPA.
         </p>
         <a
           className="App-link"
